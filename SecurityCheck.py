21f5c3b042919fb93e785ee742c65de9845886b2
afmirandadiaz <amirandadiaz@godaddy.com>
2024-09-17 09:49:53 -0500

    Tokens removed

--- SecurityCheck.py
+++ SecurityCheck.py
@@ -27,13 +27,13 @@ class IpDataResearch:
     # Metodo para obtener las región de una IP dada
     def getRegionFromIp(self):
         for ip in self.ipListByDomain:
-            responseRegion = requests.get(f"https://ipinfo.io/{ip}?token=f18cd1e491bd68")
+            responseRegion = requests.get(f"https://ipinfo.io/{ip}?token=")
             time.sleep(10)
             self.regionByIp[str(ip)] = responseRegion.json()['city']
 
     #Metodo para obtener correos desde el dominio
     def getEmailsFromDomain(self):
-        responseEmail = requests.get(f"https://api.hunter.io/v2/domain-search?domain={self.domain}&api_key=2cc9012e339a13ba2743a17cc7c60bf185684f51")
+        responseEmail = requests.get(f"https://api.hunter.io/v2/domain-search?domain={self.domain}&api_key=")
         time.sleep(10)
         responseEmailJson = responseEmail.json()
         if responseEmailJson['data']['emails'] != None:
@@ -51,62 +51,9 @@ class IpDataResearch:
         responseSave = collectionAccess.insert_one(dataFormatTemplate)
         print(responseSave.inserted_id)
 
-empresas_peru = [
-    "santander.com.pe",
-    "bcp.com.pe",
-    "interbank.pe",
-    "scotiabank.com.pe",
-    "rpp.pe",
-    "entel.pe",
-    "telefonica.com.pe",
-    "backus.pe",
-    "grupolima.com",
-    "inkaterra.com",
-    "vivaair.com.pe",
-    "petroperu.com.pe",
-    "ferreyros.com.pe",
-    "oerlikon.com",
-    "sodimac.com.pe",
-    "ripley.com.pe",
-    "bcp.com.pe",
-    "cosapi.com.pe",
-    "lap.com.pe",
-    "grupouno.pe",
-    "cencosud.pe",
-    "gildemeister.com.pe",
-    "lima.com.pe",
-    "credivarius.com.pe",
-    "mapfre.com.pe",
-    "mibanco.com.pe",
-    "peru.com",
-    "uniondebanquero.com.pe",
-    "millenium.pe",
-    "gamarra.com.pe",
-    "deltron.com.pe",
-    "entel.pe",
-    "clubelcomercio.pe",
-    "indecopi.gob.pe",
-    "klm.com.pe",
-    "gruporpp.com",
-    "agrobank.com.pe",
-    "solimano.com.pe",
-    "prosegur.com.pe",
-    "jbs.com.pe",
-    "plataformamedia.com",
-    "grupoferreyros.com.pe",
-    "brinks.com.pe",
-    "tenaris.pe",
-    "grupo-sbs.com.pe",
-    "transoceánica.com.pe",
-    "plaza-vea.com.pe",
-    "empresaeditora.com.pe",
-    "electricidaddelperu.com.pe",
-    "atv.pe",
-    "pandero.com.pe",
-    "cosapi.com.pe",
-    "topitop.com.pe",
+empresas = [
 ]
-for dominio in empresas_peru:
+for dominio in empresas:
     print(f"Dominio: {dominio}")
     try:
         prueba = IpDataResearch(dominio)
